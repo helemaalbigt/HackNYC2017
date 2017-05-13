b186d39a98a29f3aa7581b9f89264346175dc7d2
angel <sayangel@gmail.com>
2017-05-13 19:59:38 -0400

    receiving intent from snapchat

---
+++ Assets/#Project/Scripts/AndroidIntentReceiver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidIntentReceiver : MonoBehaviour {
+
+	void Awake(){
+		Debug.Log ("AWAKE");
+		string arguments = "";
+
+		AndroidJavaClass UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+		AndroidJavaObject currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+		AndroidJavaObject intent = currentActivity.Call<AndroidJavaObject>("getIntent");
+
+		//catch the EXTRA_STREAM being sent by snapchat
+		string intentKey = "android.intent.extra.STREAM";
+
+		bool hasExtra = intent.Call<bool> ("hasExtra", intentKey);
+
+		Debug.Log ("looking for " + intentKey);
+		if (hasExtra) {
+			Debug.Log ("found " + intentKey);
+			AndroidJavaObject extras = intent.Call<AndroidJavaObject> ("getExtras");
+			AndroidJavaObject URI = extras.Call<AndroidJavaObject> ("get", intentKey);
+			string URIstring = URI.Call<string> ("toString");
+			Debug.Log ("URI " + URIstring);
+		}
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
