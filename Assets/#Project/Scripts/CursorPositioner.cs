20e2ef1a801db35b7356ff8c229a0951b843406f
DESKTOP-S14HJIE\Thomas <van.bouwel.thomas@gmail.com>
2017-05-14 04:04:11 -0400

    added over, updated ui, added logo

---
+++ Assets/#Project/Scripts/CursorPositioner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorPositioner : MonoBehaviour {
+
+    public bool _followNormal = false;
+    public bool _constantScale = false;
+
+    public RaycastInput _raycast;
+    public Transform _cursor;
+    public Transform _playerEyes;
+	
+	// Update is called once per frame
+	void Update () {
+        if (_raycast.rayHit) {
+            _cursor.gameObject.SetActive(true);
+            _cursor.position = _raycast.hitPos;
+            _cursor.LookAt(_playerEyes);
+        } else {
+            _cursor.gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Assets/#Project/Scripts/HoverInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HoverInteraction : MonoBehaviour {
+
+    public UnityEvent OnHoverOn;
+    public UnityEvent OnHoverOff;
+
+	public void HoverOn() {
+        Debug.Log("hovered on");
+        OnHoverOn.Invoke();
+    }
+
+    public void HoverOff() {
+        Debug.Log("hovered off");
+        OnHoverOff.Invoke();
+    }
+}
