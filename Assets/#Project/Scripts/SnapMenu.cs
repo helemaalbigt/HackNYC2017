983c4c98344b93e820b7c4ed2bc9bafc55aafd89
DESKTOP-S14HJIE\Thomas <van.bouwel.thomas@gmail.com>
2017-05-14 08:11:51 -0400

    just commit it all

--- Assets/#Project/Scripts/SnapMenu.cs
+++ Assets/#Project/Scripts/SnapMenu.cs
@@ -6,13 +6,72 @@ public class SnapMenu : MonoBehaviour {
 
     public GameObject _buttonPrefab;
     public Transform _buttonParent;
+    public VRInput _input;
+    public SnapScreen _screen;
+    public Animator _animator;
+    public GameObject _leftArrow;
+    public GameObject _rightArrow;
+    public GameObject _instructions;
 
     private Dictionary<float, SnapButton> buttons = new Dictionary<float, SnapButton>();
+    private int _currentIndex = 0;
+    private float _targetAngle;
 
 	// Use this for initialization
 	void Start () {
         AddUIButton(); AddUIButton(); AddUIButton(); AddUIButton();
 
+        SubscribeToInput();
+    }
+
+    private void Update() {
+        //Debug.Log(360 - _buttonParent.transform.localEulerAngles.y + "-" + _targetAngle);
+        float angle = Mathf.Lerp((360 - _buttonParent.transform.localEulerAngles.y) % 360, _targetAngle, Time.deltaTime * 5);
+        _buttonParent.transform.localEulerAngles = new Vector3(0, -angle, 0);
+    }
+
+    public void Next() {
+        if(_currentIndex + 1 < buttons.Count) {
+            _currentIndex++;
+            _targetAngle = _currentIndex * 15;
+        }
+        SetArrows();
+    }
+
+    public void Prev() {
+        if (_currentIndex  > 0) {
+            _currentIndex--;
+            _targetAngle = _currentIndex * 15;
+        }
+        SetArrows();
+    }
+
+    public void HideStartScreen() {
+        Debug.Log("Hide Loading");
+        _animator.Play("Loaded");
+    }
+
+    private void SetArrows() {
+        if(_currentIndex == 0) {
+            _rightArrow.SetActive(true);
+            _leftArrow.SetActive(false);
+        } else if(_currentIndex + 1 == buttons.Count) {
+            _rightArrow.SetActive(false);
+            _leftArrow.SetActive(true);
+        } else {
+            _rightArrow.SetActive(true);
+            _leftArrow.SetActive(true);
+        }
+    }
+
+    private void HideInstructions() {
+        _instructions.SetActive(false);
+    }
+
+    private void SubscribeToInput() {
+        _input.OnSwipeRight += Prev;
+        _input.OnSwipeLeft += HideInstructions;
+        _input.OnSwipeLeft += Next;
     }
 
     private void AddUIButton() {
--- Assets/#Project/Scripts/SnapScreen.cs
+++ Assets/#Project/Scripts/SnapScreen.cs
@@ -9,37 +9,47 @@ using System.IO;
 public class SnapScreen : MonoBehaviour {
 
     public VideoPlayer _videoPlayer;
+    public SnapMenu _menu;
 
     private string filePath;
     private string fullPath;
 
     // Use this for initialization
     void Start () {
-
-        if (Application.isEditor) {
-            filePath = "./" + Application.dataPath + "/Resources/";           
-        } else {
-            filePath = "./files/Resources/";//Application.persistentDataPath + " / Resources/"
-        }
-
-        _videoPlayer.loopPointReached += Play;
-        _videoPlayer.prepareCompleted += Play;
-
-        _videoPlayer.skipOnDrop = true;
-        fullPath = filePath + "video.mp4";
-        Debug.Log("FILEPATH => " + fullPath);
-        _videoPlayer.url = fullPath;//"file:/" + 
+        SubscribeToEvents();
+        //SetUpLink();
         _videoPlayer.Prepare();
-        
     }
 
     private void Update() {
-        // Debug.Log("is playing: " + _videoPlayer.isPlaying + " is prepared: " + _videoPlayer.isPrepared);
-        
+       // Debug.Log("is playing: " + _videoPlayer.isPlaying + " is prepared: " + _videoPlayer.isPrepared);       
     }
 
     public void SetVideo(VideoClip clip) {
         _videoPlayer.clip = clip;
+    }
+
+    private void SubscribeToEvents() {
+        _videoPlayer.loopPointReached += Play;
+        _videoPlayer.prepareCompleted += Play;
+        _videoPlayer.prepareCompleted += HideStartScreen;
+    }
+
+    private void HideStartScreen(UnityEngine.Video.VideoPlayer vp) {
+        _menu.HideStartScreen();
+    }
+
+    private void SetUpLink() {
+        if (Application.isEditor) {
+            filePath = "./" + Application.dataPath + "/Resources/";
+        } else {
+            filePath = "./files/Resources/";//Application.persistentDataPath + " / Resources/"
+        }
+        _videoPlayer.url = fullPath;//"file:/" 
+        Debug.Log("FILEPATH => " + fullPath);
+
+        _videoPlayer.skipOnDrop = true;
+        fullPath = filePath + "video.mp4";
     }
 
     private void Play(UnityEngine.Video.VideoPlayer vp) {
