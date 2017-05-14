49d9639df82e2361434a0df762dd4899cf952baa
angel <sayangel@gmail.com>
2017-05-14 01:26:22 -0400

    kind of working

--- Assets/#Project/Scripts/AndroidIntentReceiver.cs
+++ Assets/#Project/Scripts/AndroidIntentReceiver.cs
@@ -10,6 +10,7 @@ public class AndroidIntentReceiver : MonoBehaviour {
 
 		AndroidJavaClass UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
 		AndroidJavaObject currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
 
 		AndroidJavaObject intent = currentActivity.Call<AndroidJavaObject>("getIntent");
 
@@ -21,13 +22,127 @@ public class AndroidIntentReceiver : MonoBehaviour {
 		Debug.Log ("looking for " + intentKey);
 		if (hasExtra) {
 			Debug.Log ("found " + intentKey);
+
+			//Get Content Stream URI
 			AndroidJavaObject extras = intent.Call<AndroidJavaObject> ("getExtras");
 			AndroidJavaObject URI = extras.Call<AndroidJavaObject> ("get", intentKey);
+
 			string URIstring = URI.Call<string> ("toString");
-			Debug.Log ("URI " + URIstring);
+			Debug.Log ("URI STRING " + URIstring);
+
+			//Resolve Content URI to File URI
+			AndroidJavaObject contentResolver = context.Call<AndroidJavaObject>("getContentResolver");
+			AndroidJavaObject cursor = contentResolver.Call<AndroidJavaObject> ("query", URI, null, null, null, null);
+			cursor.Call<bool>("moveToFirst");
+
+			string filePath = cursor.Call<string> ("getString", 0);
+			cursor.Call ("close");
+
+			Debug.Log ("file path " + filePath);
+
+			//Save file provider info to file
+
+			// mInputPFD = getContentResolver().openFileDescriptor(mVideoUri, "r");
+			AndroidJavaObject inputFileDesc = contentResolver.Call<AndroidJavaObject> ("openFileDescriptor", URI, "r"); 
+
+			// FileDescriptor fd = mInputPFD.getFileDescriptor();
+			AndroidJavaObject fd = inputFileDesc.Call<AndroidJavaObject>("getFileDescriptor");
+
+			//InputStream inputStream = new FileInputStream(fd);
+			AndroidJavaObject inputStream = new AndroidJavaObject("java.io.FileInputStream", fd);
+
+			//File targetFile = new File(Environment.getExternalStorageDirectory(), fileName);
+			AndroidJavaObject env = new AndroidJavaObject("android.os.Environment");
+			AndroidJavaObject externalStorageDir = env.CallStatic<AndroidJavaObject>("getExternalStorageDirectory");
+
+			AndroidJavaObject targetFile = new AndroidJavaObject ("java.io.File", externalStorageDir, filePath);
+
+			//FileOutputStream fileOutputStream = new FileOutputStream(targetFile);
+			AndroidJavaObject fileOutputStream = new AndroidJavaObject("java.io.FileOutputStream", targetFile);
+
+			//byte[] buffer = new byte[inputStream.available()];
+			int availableBytes = inputStream.Call<int>("available");
+			//javaByteArrayFromCS (availableBytes);
+
+			Debug.Log ("creating byte array of size " + availableBytes);
+			AndroidJavaClass arrayClass  = new AndroidJavaClass("java.lang.reflect.Array");
+			AndroidJavaObject arrayObject = arrayClass.CallStatic<AndroidJavaObject>("newInstance", new AndroidJavaClass("java.lang.Byte"), availableBytes);
+
+			//initialize to all 0s
+			for (int i=0; i< availableBytes; ++i) {
+				arrayClass.CallStatic("set", arrayObject, i, new AndroidJavaObject("java.lang.Byte", 0));
+			}
+
+			AndroidJavaObject buffer = arrayObject;
+			int arrayLength = arrayObject.CallStatic<int>("length");
+
+			Debug.Log("array length after creation " + arrayLength);
+
+			//inputStream.read(buffer);
+			inputStream.Call<int>("read", buffer);
+
+			//fileOutputStream.write(buffer);
+			fileOutputStream.Call("write", buffer);
+
+			//close streams
+			inputStream.Call("close");
+			fileOutputStream.Call ("close");
+			inputFileDesc.Call ("close");
+
+			//save buffer to mediaprovider
+
+			//set values
+			string fileAbsolutePath = targetFile.Call<string>("getAbsolutePath");
+
+			//ContentValues values = new ContentValues(3);
+			AndroidJavaObject values = new AndroidJavaObject ("android.content.ContentValues", 3);
+
+			/*values.put(MediaStore.Video.Media.TITLE, "My video title");
+			values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
+			values.put(MediaStore.Video.Media.DATA, videoFile.getAbsolutePath());*/
+
+			values.Call("put", "title", "Snapchat Video");
+			values.Call ("put", "mime_type", "video/mp4");
+			values.Call ("put", "_data", fileAbsolutePath);
+
+			Debug.Log ("absolute path " + fileAbsolutePath);
+
+			//return getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
+			//AndroidJavaObject mediaStore = new AndroidJavaObject("android.provider.MediaStore");
+			//AndroidJavaObject mediaStoreVideoMedia = mediaStore.GetStatic<AndroidJavaObject>("Video");
+			//AndroidJavaObject externalContentURI = mediaStoreVideoMedia.GetStatic<AndroidJavaObject> ("EXTERNAL_CONTENT_URI");
+
+			//this should call MediaStore.Video.Media.EXTERNAL_CONTENT_URI 
+			//but I couldn't figure out how to call the nested static classes... so hard coded.
+			string externalContentURIString = "content://media/external/video/media";
+			AndroidJavaObject AndroidURI = new AndroidJavaObject ("android.net.Uri");
+			Debug.Log ("parsing...");
+			AndroidJavaObject externalContentURI = AndroidURI.CallStatic<AndroidJavaObject> ("parse", externalContentURIString);
+			string parsedString = externalContentURI.Call<string> ("toString");
+			Debug.Log("parsed external content URI as " + parsedString);
+			Debug.Log ("creating final URI...");
+
+			AndroidJavaObject finalURI = contentResolver.Call<AndroidJavaObject> ("insert", externalContentURI, values);
+			Debug.Log("inserted!");
+			string videoURI = finalURI.Call<string> ("toString");
+			string videoPath = finalURI.Call<string> ("getPath");
+
+			Debug.Log ("videoUri: " + videoURI);
+			Debug.Log("videoPath: " + videoPath);
+
 		}
 
 
+	}
+
+	private AndroidJavaObject javaByteArrayFromCS(int size) {
+		Debug.Log ("creating byte array of size " + size);
+		AndroidJavaClass arrayClass  = new AndroidJavaClass("java.lang.reflect.Array");
+		AndroidJavaObject arrayObject = arrayClass.CallStatic<AndroidJavaObject>("newInstance",
+			new AndroidJavaClass("java.lang.Byte"),
+			size);
+
+		return arrayObject;
 	}
 
 	// Use this for initialization
